f0afb96be8d4b52b9a4c0aa4613ead63bc618083
Diogo1191 <diogo.ferreira.50059@my.istec.pt>
2024-06-10 20:34:31 +0100

    Commit
    Ja coloquei os construtores no Hardware e no Ticket

--- Models/HardwareTicket.py
+++ Models/HardwareTicket.py
@@ -1,8 +1,10 @@
 from Utils.db import Database
-from Models.Ticket import Ticket
+from Models.ticket import Ticket
+from datetime import datetime
 
 class HardwareTicket(Ticket):
     
+    '''
     def __init__ (self,equipamento,avaria,dataAtendimento,descricaoReparacao,pecas,estado):
         super.__init__(id,'porAtender','hardware')
         self.equipamento = equipamento
@@ -11,6 +13,33 @@ class HardwareTicket(Ticket):
         self.descricaoReparacao = descricaoReparacao
         self.pecas = pecas
         self.estado = estado
+    ''' 
+
+    def __init__ (self, *args):
+        if len(args) == 1 and isinstance(args[0], HardwareTicket):
+            super().__init__(args[0])           
+            self.equipamento = args[0].equipamento
+            self.avaria = args[0].avaria
+            self.dataAtendimento = args[0].dataAtendimento
+            self.descricaoReparacao = args[0].descricaoReparacao
+            self.pecas = args[0].pecas
+            self.estado = args[0].estado
+        elif len(args) == 10:
+            super().__init__(args[0], args[1], args[2], args[3])
+            self.equipamento = args[4]
+            self.avaria = args[5]
+            self.dataAtendimento = args[6]
+            self.descricaoReparacao = args[7]
+            self.pecas = args[8]
+            self.estado = args[9]
+        else:
+            super().__init__(args[0], args[1], args[2], args[3])
+            self.equipamento = None
+            self.dataAtendimento = datetime.now()
+            self.descricaoReparacao = None
+            self.pecas = None
+            self.estado = 'Aberto'
+
         
         def save(self):
             db = Database()
--- Models/ticket.py
+++ Models/ticket.py
@@ -1,12 +1,39 @@
 from Utils.db import Database
+from datetime import datetime
+
 
 class Ticket:
     
+    """
     def __init__(self, data_hora, codigo_colaborador, estado_ticket,tipo_ticket):
         self.data_hora = data_hora
         self.codigo_colaborador = codigo_colaborador
         self.estado_ticket = estado_ticket
         self.tipo_ticket = tipo_ticket
+    """
+
+    def __init__(self, *args):
+        #(Construtor Cópia)
+        if len(args) == 1 and isinstance(args[0], Ticket):
+            self.data_hora = args[0].data_hora
+            self.codigo_colaborador = args[0].codigo_colaborador
+            self.estado_ticket = args[0].estado_ticket
+            self.tipo_ticket = args[0].tipo_ticket
+        elif len(args) == 4:
+            #Construtor por Parametros
+            self.data_hora = args[0]
+            self.codigo_colaborador = args[1]
+            self.estado_ticket = args[2]
+            self.tipo_ticket = args[3]
+        else: 
+            #(Construtor Default)
+            self.data_hora = datetime.now()
+            self.codigo_colaborador = 0
+            self.estado_ticket = "porAtender"
+            self.tipo_ticket = None
+
+
+
 
     def save(self):
         db = Database()
